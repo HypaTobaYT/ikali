c63a543ce18a7d194ccc7940645b76017c4ad840
HypaTobaYT <108157483+HypaTobaYT@users.noreply.github.com>
2026-01-25 09:47:43 +0000

    Update version to v1.0.10 in setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ikali",
-    version="v1.0.9",
+    version="v1.0.10",
     author="Hypa",
     author_email="hypertobayt@gmail.com",
     description="iKali, runs a command when typing ikali into the terminal.",
@@ -52,3 +52,4 @@ setup(
         'install': PostInstallCommand,
     },
 )
+
